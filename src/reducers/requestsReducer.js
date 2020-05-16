646c260c5f21c39db52c8c877b24cfbdef868c78
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-15 23:53:19 -0700

    requestsReducer handles MAKE_REQUEST action

--- src/reducers/requestsReducer.js
+++ src/reducers/requestsReducer.js
@@ -1,5 +1,9 @@
+import { MAKE_REQUEST } from '../actions/action';
+
 export default function reducer(state = [], action) {
   switch(action.type) {
+    case MAKE_REQUEST:
+      return [action.payload, ...state];
     default:
       return state;
   }

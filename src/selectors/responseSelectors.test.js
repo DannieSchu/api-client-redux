421b04638c44d0588b0efe61e27cccc51463f690
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-17 23:19:08 -0700

    write failing test for getLoading selector

--- src/selectors/responseSelectors.test.js
+++ src/selectors/responseSelectors.test.js
@@ -1,4 +1,4 @@
-import { getResponse } from './responseSelectors';
+import { getResponse, getLoading } from './responseSelectors';
 
 describe('response selectors', () => {
   it('selects response state', () => {
@@ -16,5 +16,19 @@ describe('response selectors', () => {
       quantity: 1,
       results: [{ name: 'Jessica' }]
     });
+  });
+
+  it('selects loading state', () => {
+    const state = {
+      loading: false,
+      response: {
+        quantity: 1,
+        results: [{ name: 'Jessica' }]
+      }
+    };
+
+    const loadingState = getLoading(state);
+
+    expect(loadingState).toEqual(false);
   });
 });

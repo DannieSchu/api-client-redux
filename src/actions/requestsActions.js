51d4c3f54a72e66536117ec843154645413582cc
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-18 00:40:49 -0700

    SET_REQUESTS action for storing requests in local storage

--- src/actions/requestsActions.js
+++ src/actions/requestsActions.js
@@ -1,11 +1,10 @@
-export const ADD_REQUEST = 'ADD_REQUEST';
-export const addRequest = request => ({
-  type: ADD_REQUEST,
-  payload: request
-});
-
 export const SET_REQUESTS = 'SET_REQUESTS';
-export const setRequests = requests => ({
-  type: SET_REQUESTS,
-  payload: requests
-});
+export const setRequests = requests => dispatch => {
+  localStorage.setItem('requests', JSON.stringify(requests))
+    .then(requests => {
+      dispatch({
+        type: SET_REQUESTS,
+        payload: requests
+      });
+    });
+};

fab2b6df77257f563671cd70c76a51281fe82da9
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-17 23:57:38 -0700

    remove context provider from App

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
-import { APIClientProvider } from '../../hooks/APIClientProvider';
 
 describe('App component', () => {
   it('renders App', () => {
     const wrapper = shallow(
-      <APIClientProvider>
-        <App />
-      </APIClientProvider>
+      <App />
     );
     expect(wrapper).toMatchSnapshot();
   });

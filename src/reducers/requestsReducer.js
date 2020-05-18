12b8410c8e9b4049776d7ae4e8ce998b7b4fc7ab
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-18 00:42:51 -0700

    add SET_REQUESTS action to requestsReducer

--- src/reducers/requestsReducer.js
+++ src/reducers/requestsReducer.js
@@ -1,9 +1,12 @@
 import { MAKE_REQUEST } from '../actions/action';
+import { SET_REQUESTS } from '../actions/requestsActions';
 
 export default function reducer(state = [], action) {
   switch(action.type) {
     case MAKE_REQUEST:
       return [action.payload, ...state];
+    case SET_REQUESTS:
+      return action.payload;
     default:
       return state;
   }
